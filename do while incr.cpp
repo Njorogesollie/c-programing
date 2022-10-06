20a8d1d55b025294dea7f9ba62224c9fd7372bd9
Njorogesollie <113591924+Njorogesollie@users.noreply.github.com>
2022-10-06 08:35:34 +0300

    Add files via upload

---
+++ do while incr.cpp
@@ -0,0 +1,13 @@
+//program for displaying 10 numbers
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv)//main function
+{
+	int i=1;
+	do {
+		cout<<i<<endl;//code executed
+		i++;
+	}
+	while(i<=10);//condition
+	return 0;
+}
